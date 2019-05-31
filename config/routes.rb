d1c445d7cdde17a1b79ebea3abd1cbddc1229d10
github username <github email address>
2019-05-31 10:07:40 -0600

    added root

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :admins
   resources :users
   resources :messages
+  root  'controller_name#index' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
